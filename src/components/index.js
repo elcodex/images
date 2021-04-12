fa95bdfc5b6924c4564669c182800d1644ee60b1
Elen <elcrses@ya.ru>
2021-04-12 21:38:33 +0300

    index.js for components

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as DropFiles } from './DropFiles/DropFiles';
+export { default as ButtonAddFiles } from './ButtonAddFiles/ButtonAddFiles';
+export { default as ImagesCollection } from './ImagesCollection/ImagesCollection';
+export { default as ButtonClear } from './ButtonClear/ButtonClear';
