effbc2a30820d9067bda3102dc48b56f1fc73c59
Elen <elcrses@ya.ru>
2021-04-15 17:44:45 +0300

    components/index: rename components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 export { default as DropFiles } from './DropFiles/DropFiles';
-export { default as ButtonAddFiles } from './ButtonAddFiles/ButtonAddFiles';
+export { default as ButtonUpload } from './ButtonUpload/ButtonUpload';
 export { default as ImagesCollection } from './ImagesCollection/ImagesCollection';
 export { default as ButtonClear } from './ButtonClear/ButtonClear';
-export { default as InputURL } from './InputURL/InputURL';
-export { default as LoadPanel } from './LoadPanel/LoadPanel';
+export { default as UploadPanel } from './UploadPanel/UploadPanel';
 export { default as Gallery } from './Gallery/Gallery';
